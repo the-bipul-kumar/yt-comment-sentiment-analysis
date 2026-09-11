769d92af95c65b2c13febafb22a5dea45f102886
Bipul Kumar Sahu <bipulkumarsahu04012003@example.com>
2026-09-11 10:00:30 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A chrome plugin to detect youtube comment sentiments',
+    author='Bipul',
+    license='',
+)
